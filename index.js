b7ad148d7e864f5f354decbaef6d42b50b8c5760
Logan Franken <loganfranken@gmail.com>
2021-02-24 21:14:22 -0800

    Added basic Koa routing example

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import config from './config.js';
 import Koa from 'koa';
+import Router from '@koa/router';
 import yelp from 'yelp-fusion';
 
 const app = new Koa();
+const router = new Router();
+const yelpClient = yelp.client(config.apiKey);
 
-app.use(async ctx => {
+router.get('/api/businesses', async (ctx) => {
 
-  const client = yelp.client(config.apiKey);
-  const response = await client.search({
+  const response = await yelpClient.search({
     term: 'gluten free',
     latitude: '47.606209',
     longitude: '-122.332069'
@@ -16,5 +18,9 @@ app.use(async ctx => {
   ctx.body = response.jsonBody.businesses[0].name;
 
 });
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
 
 app.listen(config.port);
