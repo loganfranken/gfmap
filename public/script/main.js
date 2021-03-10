c330bd762a1f84faf81774a08949b9429923efbc
Logan Franken <loganfranken@gmail.com>
2021-03-09 22:53:56 -0800

    Basic React integration and webpack config

--- public/script/main.js
+++
@@ -1,6 +0,0 @@
-const name = "Test";
-
-ReactDOM.render(
-    <h1>Hello, {name}!</h1>,
-    document.getElementById('root')
-)
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+import 'webpack';
+import path from 'path';
+
+export default {
+  entry: './public/script/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('./public/script'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx'
+    ]
+  }
+};
